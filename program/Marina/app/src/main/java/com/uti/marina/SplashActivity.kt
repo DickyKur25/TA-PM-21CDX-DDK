b7e5e32548c081475a421fdcdcbf2d58fb95b8cc
DickyKur25 <dicky_kurniawan@teknokrat.ac.id>
2024-06-15 17:34:47 +0700

    dicky: atur agar splash activity erjalan 3 detik

--- program/Marina/app/src/main/java/com/uti/marina/SplashActivity.kt
+++ program/Marina/app/src/main/java/com/uti/marina/SplashActivity.kt
@@ -1,20 +1,23 @@
 package com.uti.marina
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // Delay 3 detik
     }
 }
