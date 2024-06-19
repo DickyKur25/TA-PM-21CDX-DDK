22dbfa02168a02e5951d4a1ee500dc476e9569f9
DickyKur25 <dicky_kurniawan@teknokrat.ac.id>
2024-06-19 17:29:18 +0700

    dicky: sinkronasi agar button tampil beta bisa ke maps di LokasiActivity.kt

--- program/Marina/app/src/main/java/com/uti/marina/LokasiActivity.kt
+++ program/Marina/app/src/main/java/com/uti/marina/LokasiActivity.kt
@@ -1,20 +1,25 @@
 package com.uti.marina
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LokasiActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lokasi)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // ... (kode sebelumnya untuk mengatur padding) ...
+
+        findViewById<View>(R.id.btnTampilPeta).setOnClickListener { view ->
+            val urllokasi = Uri.parse("google.navigation:q=Pantai Marina, Kalianda Lampung Selatan")
+            val pindah = Intent(Intent.ACTION_VIEW, urllokasi)
+            startActivity(pindah)
         }
     }
 }
