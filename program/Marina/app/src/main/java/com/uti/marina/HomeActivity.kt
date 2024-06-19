da73f5e8c80b491422ecabf30e6e3dbcffabf818
desifitriaa <desi_fitria@teknokrat.ac.id>
2024-06-19 23:58:31 +0700

    desifitriaa:mengatur onclik

--- program/Marina/app/src/main/java/com/uti/marina/HomeActivity.kt
+++ program/Marina/app/src/main/java/com/uti/marina/HomeActivity.kt
@@ -1,8 +1,10 @@
-package com.uti.marina
+package com.uti.marina // Sesuaikan dengan package Anda
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.uti.marina.LokasiActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,16 @@ class HomeActivity : AppCompatActivity() {
         // Tambahkan kode untuk inisialisasi tampilan dan logika di HomeActivity
     }
 
-    fun Galeri(view: View) {}
+    fun tampilkanLokasi(view: View) {val intent = Intent(this, LokasiActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun tampilkanPaket(view: View) {val intent = Intent(this, PaketActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun tampilkanGaleri(view: View) {val intent = Intent(this, GaleriActivity::class.java)
+        startActivity(intent)
+    }
+
 }
