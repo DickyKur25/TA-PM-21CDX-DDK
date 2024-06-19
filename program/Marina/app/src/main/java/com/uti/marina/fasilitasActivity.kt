a2074e571e72b76fa65f22697cf4c21e927cbfb9
kharismaseptianii <kharismaseptiani94@gmail.com>
2024-06-19 18:11:33 +0700

    kharismaseptianii: menambahkan layout baru, fasilitas activity

--- program/Marina/app/src/main/java/com/uti/marina/fasilitasActivity.kt
+++ program/Marina/app/src/main/java/com/uti/marina/fasilitasActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class fasilitas_activity : AppCompatActivity() {
+class fasilitasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
