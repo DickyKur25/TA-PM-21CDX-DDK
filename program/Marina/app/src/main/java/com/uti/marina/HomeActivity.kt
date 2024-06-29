0e78e373885f0f7a06b7681ced36c24f93ce3582
desifitriaa <desi_fitria@teknokrat.ac.id>
2024-06-29 16:12:10 +0700

    desifitriaa: menambahkan onclik pada galeri, membuata activity about, mengubah beberapa gambar di activity_home

--- program/Marina/app/src/main/java/com/uti/marina/HomeActivity.kt
+++ program/Marina/app/src/main/java/com/uti/marina/HomeActivity.kt
@@ -1,29 +1,21 @@
-package com.uti.marina // Sesuaikan dengan package Anda
+package com.uti.marina
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import com.uti.marina.LokasiActivity
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // Tambahkan kode untuk inisialisasi tampilan dan logika di HomeActivity
+        // Inisialisasi tombol dan set onClick listener
+        val galeriButton: Button = findViewById(R.id.button3)
+        galeriButton.setOnClickListener {
+            val intent = Intent(this, GaleriActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-    fun tampilkanLokasi(view: View) {val intent = Intent(this, LokasiActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun tampilkanPaket(view: View) {val intent = Intent(this, PaketActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun tampilkanGaleri(view: View) {val intent = Intent(this, GaleriActivity::class.java)
-        startActivity(intent)
-    }
-
 }
